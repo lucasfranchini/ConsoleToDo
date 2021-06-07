35355779a2263c44172efdc56361afa98581361e
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-07 17:49:15 -0300

    creating functions for add, remove e show the todo list

--- ToDo.js
+++ ToDo.js
@@ -1,4 +1,29 @@
 import readlineSync from 'readline-sync';
 
-const options = ['add','list','check','remove'];
-let action = readlineSync.keyInSelect(options,'o que voce quer fazer?');
+const options = ['add', 'list', 'check', 'remove'];
+let list = [];
+let action = readlineSync.keyInSelect(options, 'Type your command ');
+while (action !== -1) {
+    if (action === 0) {
+        list.push('🔴 '+readlineSync.question('what do you want to do? '));
+        console.log(list)
+    }
+    if (action === 1) {
+        console.log('\n======================');
+        for (let i=0;i<list.length;i++){
+            console.log(list[i])
+        }
+        console.log('======================');
+    }
+    if (action === 2) {
+        const verify = readlineSync.keyInSelect(list, 'what do you want to check/uncheck? ');
+        list[verify].replace('🔴',"🟢");
+        console.log(list[verify]);
+    }
+    if (action === 3) {
+        const verify = readlineSync.keyInSelect(list, 'what do you want to check/uncheck? ');
+        list.splice(verify,1);
+        console.log(list)
+    }
+    action = readlineSync.keyInSelect(options, 'Type your command ');
+}
