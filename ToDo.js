8b776f1c71e71275acbd1e08bd710fc3db4d8bde
lucasfranchini <lucasfranchini.a@gmail.com>
2021-06-07 18:02:20 -0300

    creating a component with the showList function

--- ToDo.js
+++ ToDo.js
@@ -1,4 +1,5 @@
 import readlineSync from 'readline-sync';
+import showList from './showList.js';
 
 const options = ['add', 'list', 'check', 'remove'];
 let list = ['🔴 a'];
@@ -8,20 +9,18 @@ while (action !== -1) {
         list.push('🔴 '+readlineSync.question('what do you want to do? '));
     }
     if (action === 1) {
-        console.log('\n======================');
-        for (let i=0;i<list.length;i++){
-            console.log(list[i])
-        }
-        console.log('======================');
+        showList(list);
     }
     if (action === 2) {
         const verify = readlineSync.keyInSelect(list, 'what do you want to check/uncheck? ');
         if(list[verify].indexOf('🔴')!==-1) list[verify] = list[verify].replace('🔴',"🟢");
         else list[verify] = list[verify].replace("🟢",'🔴');
+        showList(list);
     }
     if (action === 3) {
         const verify = readlineSync.keyInSelect(list, 'what do you want to remove? ');
         list.splice(verify,1);
+        showList(list);
     }
     action = readlineSync.keyInSelect(options, 'Type your command ');
 }
---
+++ showList.js
@@ -0,0 +1,7 @@
+export default function showList(list) {
+    console.log('\n======================');
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i])
+    }
+    console.log('======================');
+}
